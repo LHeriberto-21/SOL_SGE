6d3800c1e6da8df6b12da8f89c4cafc985b34f92
LHeriberto21 <hl4229705@gmail.com>
2025-07-28 14:39:20 -0700

    Generacion de clases

--- SOL_SGE/Service/UserSesion.cs
+++ SOL_SGE/Service/UserSesion.cs
@@ -12,7 +12,7 @@ namespace SOL_SGE.Service
         public static string Username { get; set; }
         public static string Rol {get; set;}
         public static string Email { get; set; }
-
+            
         public static void IniciarSesion(string username, string rol, string email)
         {
             Username = username;
